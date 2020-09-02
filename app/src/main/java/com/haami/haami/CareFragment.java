43ca940301ec35075026c6bb30d7f0782a5d5765
Sadra <sadra@united4iran.org>
2020-09-02 20:45:45 +0300

    Add beginner and advance level for Care

--- app/src/main/java/com/haami/haami/CareFragment.java
+++ app/src/main/java/com/haami/haami/CareFragment.java
@@ -9,7 +9,9 @@ import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 
 import com.haami.haami.R;
@@ -20,6 +22,9 @@ public class CareFragment extends Fragment implements View.OnClickListener {
     SeekBar seekBar;
     MediaPlayer player;
     Boolean isPlaying = false;
+    ImageView image;
+    Button beginner;
+    Button advance;
 
     private Handler mHandler = new Handler();
 
@@ -45,9 +50,15 @@ public class CareFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        play_button = getView().findViewById(R.id.play_button);
         seekBar = getView().findViewById(R.id.seekBar);
+        play_button = getView().findViewById(R.id.play_button);
+        beginner = getView().findViewById(R.id.beginner_care_button);
+        advance = getView().findViewById(R.id.advance_care_button);
+        image = getView().findViewById(R.id.player_background_view);
+
         play_button.setOnClickListener(this);
+        beginner.setOnClickListener(this);
+        advance.setOnClickListener(this);
 
         getActivity().runOnUiThread(new Runnable() {
             @Override
@@ -90,6 +101,20 @@ public class CareFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.beginner_care_button:
+                image.setImageDrawable(getResources().getDrawable(R.drawable.mindfulness_meditation_begginer));
+                beginner.setBackground(getResources().getDrawable(R.drawable.breathing_active_button_background));
+                advance.setBackground(getResources().getDrawable(R.drawable.breathing_deactive_button_background));
+                beginner.setTextColor(getResources().getColor(R.color.colorRelaxationActiveTitle));
+                advance.setTextColor(getResources().getColor(R.color.colorRelaxationTitleText));
+                break;
+            case R.id.advance_care_button:
+                image.setImageDrawable(getResources().getDrawable(R.drawable.mindfulness_meditation_advance));
+                beginner.setBackground(getResources().getDrawable(R.drawable.breathing_deactive_button_background));
+                advance.setBackground(getResources().getDrawable(R.drawable.breathing_active_button_background));
+                beginner.setTextColor(getResources().getColor(R.color.colorRelaxationTitleText));
+                advance.setTextColor(getResources().getColor(R.color.colorRelaxationActiveTitle));
+                break;
             case R.id.play_button:
                 if(!isPlaying) {
                     if (player == null) {
