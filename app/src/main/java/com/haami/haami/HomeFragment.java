1d3618488b9cf011ab5185f13931e745cb55ba82
Sadra <sadra@united4iran.org>
2020-09-02 20:46:51 +0300

    Handle sign levels and gauge level

--- app/src/main/java/com/haami/haami/HomeFragment.java
+++ app/src/main/java/com/haami/haami/HomeFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.Gravity;
@@ -30,7 +31,7 @@ import static android.content.Context.MODE_PRIVATE;
 
 public class HomeFragment extends Fragment implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
 
-    private RelativeLayout back_dim_layout;
+    private ConstraintLayout back_dim_layout;
     private PopupWindow popupWindow;
     private View popupLayout;
     private TextView day_text;
@@ -39,6 +40,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener, Date
     private ImageView sign_not_choose_image;
     private CustomGauge gauge;
     private ImageButton sign_button;
+    private int value;
 
     public HomeFragment() {
 
@@ -47,6 +49,12 @@ public class HomeFragment extends Fragment implements View.OnClickListener, Date
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         return fragment;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        showClock();
     }
 
     @Override
@@ -94,7 +102,10 @@ public class HomeFragment extends Fragment implements View.OnClickListener, Date
         editor.putInt("month", monthOfYear);
         editor.putInt("day", dayOfMonth);
         editor.commit();
-        showClock();
+        //showClock();
+        Intent splashIntent = new Intent(getActivity(), SplashActivity.class);
+        startActivity(splashIntent);
+        getActivity().finish();
     }
 
     public void showClock() {
@@ -113,14 +124,24 @@ public class HomeFragment extends Fragment implements View.OnClickListener, Date
         } else {
             int month = sharedPreferences.getInt("month", 0);
             int day = sharedPreferences.getInt("day", 0);
-            int value = compareDates(year, month, day);
-            gauge.setPointSize(value);
+            value = compareDates(year, month, day);
+            int circle_value = value * 270 / 180;
+            gauge.setPointSize(Math.min(circle_value, 270));
             sign_not_choose_image.setVisibility(View.GONE);
             set_clock_button.setVisibility(View.GONE);
             sign_button.setVisibility(View.VISIBLE);
             days_count.setVisibility(View.VISIBLE);
             day_text.setVisibility(View.VISIBLE);
             days_count.setText(String.valueOf(value));
+            if (value < 31) {
+                sign_button.setImageDrawable(getResources().getDrawable(R.drawable.rahro_active_sign));
+            } else if (value > 30 && value < 91) {
+                sign_button.setImageDrawable(getResources().getDrawable(R.drawable.rahnavard_active_sign));
+            } else if (value > 90 && value < 181) {
+                sign_button.setImageDrawable(getResources().getDrawable(R.drawable.pishtaz_active_sign));
+            } else if (value > 180) {
+                sign_button.setImageDrawable(getResources().getDrawable(R.drawable.pirouz_active_sign));
+            }
         }
     }
 
@@ -148,10 +169,12 @@ public class HomeFragment extends Fragment implements View.OnClickListener, Date
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.just_for_today_button:
-                ((MainActivity) getActivity()).replaceFragments(TodayFragment.class, null);
+                if (((MainActivity) getActivity()).checkNetworkExists())
+                    ((MainActivity) getActivity()).replaceFragments(TodayFragment.class, null);
                 break;
             case R.id.not_feeling_good_button:
-                ((MainActivity) getActivity()).replaceFragments(NotFeelingGoodFragment.class, null);
+                if (((MainActivity) getActivity()).checkNetworkExists())
+                    ((MainActivity) getActivity()).replaceFragments(NotFeelingGoodFragment.class, null);
                 break;
             case R.id.sign_button:
                 back_dim_layout.setVisibility(View.VISIBLE);
@@ -168,6 +191,46 @@ public class HomeFragment extends Fragment implements View.OnClickListener, Date
                         popupWindow.dismiss();
                     }
                 });
+                ImageView current_sign = popupWindow.getContentView().findViewById(R.id.current_sign);
+                ImageView rahro_sign = popupWindow.getContentView().findViewById(R.id.rahro_sign);
+                ImageView rahnavard_sign = popupWindow.getContentView().findViewById(R.id.rahnavard_sign);
+                ImageView pishtaz_sign = popupWindow.getContentView().findViewById(R.id.pishtaz_sign);
+                ImageView pirouz_sign = popupWindow.getContentView().findViewById(R.id.pirouz_sign);
+                TextView textView = popupWindow.getContentView().findViewById(R.id.textView);
+                TextView description_text = popupWindow.getContentView().findViewById(R.id.description_text);
+                if (value < 31) {
+                    current_sign.setImageDrawable(getResources().getDrawable(R.drawable.rahro_sign_big));
+                    rahro_sign.setImageDrawable(getResources().getDrawable(R.drawable.rahro_active_sign));
+                    rahnavard_sign.setImageDrawable(getResources().getDrawable(R.drawable.rahnavard_sign));
+                    pishtaz_sign.setImageDrawable(getResources().getDrawable(R.drawable.pishtaz_sign));
+                    pirouz_sign.setImageDrawable(getResources().getDrawable(R.drawable.pirouz_sign));
+                    textView.setText("رهرو");
+                    description_text.setText("توضیح نشان رهرو");
+                } else if (value > 30 && value < 91) {
+                    current_sign.setImageDrawable(getResources().getDrawable(R.drawable.rahnavard_sign_big));
+                    rahro_sign.setImageDrawable(getResources().getDrawable(R.drawable.rahro_sign));
+                    rahnavard_sign.setImageDrawable(getResources().getDrawable(R.drawable.rahnavard_active_sign));
+                    pishtaz_sign.setImageDrawable(getResources().getDrawable(R.drawable.pishtaz_sign));
+                    pirouz_sign.setImageDrawable(getResources().getDrawable(R.drawable.pirouz_sign));
+                    textView.setText("رهنورد");
+                    description_text.setText("توضیح نشان رهنورد");
+                } else if (value > 90 && value < 181) {
+                    current_sign.setImageDrawable(getResources().getDrawable(R.drawable.pishtaz_sign_big));
+                    rahro_sign.setImageDrawable(getResources().getDrawable(R.drawable.rahro_sign));
+                    rahnavard_sign.setImageDrawable(getResources().getDrawable(R.drawable.rahnavard_sign));
+                    pishtaz_sign.setImageDrawable(getResources().getDrawable(R.drawable.pishtaz_active_sign));
+                    pirouz_sign.setImageDrawable(getResources().getDrawable(R.drawable.pirouz_sign));
+                    textView.setText("پیشتاز");
+                    description_text.setText("توضیح نشان پشتاز");
+                } else if (value > 180) {
+                    current_sign.setImageDrawable(getResources().getDrawable(R.drawable.pirouz_sign_big));
+                    rahro_sign.setImageDrawable(getResources().getDrawable(R.drawable.rahro_sign));
+                    rahnavard_sign.setImageDrawable(getResources().getDrawable(R.drawable.rahnavard_sign));
+                    pishtaz_sign.setImageDrawable(getResources().getDrawable(R.drawable.pishtaz_sign));
+                    pirouz_sign.setImageDrawable(getResources().getDrawable(R.drawable.pirouz_active_sign));
+                    textView.setText("پیروز");
+                    description_text.setText("توضیح نشان پیروز");
+                }
                 break;
             case R.id.setting_button:
                 Intent settingActivity = new Intent(getActivity(), SettingActivity.class);
