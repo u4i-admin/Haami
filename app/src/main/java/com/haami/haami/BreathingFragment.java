e6e125765f11047de119c96c43563aa24e974295
Sadra <sadra@united4iran.org>
2020-09-10 15:15:29 +0300

    Add Breathing gif images

--- app/src/main/java/com/haami/haami/BreathingFragment.java
+++ app/src/main/java/com/haami/haami/BreathingFragment.java
@@ -1,5 +1,7 @@
 package com.haami.haami;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,13 +10,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
+import com.bumptech.glide.Glide;
 import com.haami.haami.R;
 
 public class BreathingFragment extends Fragment implements View.OnClickListener {
 
     Button advance_breathing_button;
     Button beginner_breathing_button;
+    ImageView imageView;
 
     public BreathingFragment() {
 
@@ -40,6 +47,9 @@ public class BreathingFragment extends Fragment implements View.OnClickListener 
     public void onViewCreated(View view, Bundle savedInstanceState) {
         advance_breathing_button = getView().findViewById(R.id.advance_breathing_button);
         beginner_breathing_button = getView().findViewById(R.id.beginner_breathing_button);
+        imageView = getView().findViewById(R.id.imageView);
+
+        Glide.with(this).load(R.drawable.breathing_a).into(imageView);
 
         advance_breathing_button.setOnClickListener(this);
         beginner_breathing_button.setOnClickListener(this);
@@ -52,15 +62,19 @@ public class BreathingFragment extends Fragment implements View.OnClickListener 
                 advance_breathing_button.setBackground(getResources().getDrawable(R.drawable.breathing_active_button_background));
                 beginner_breathing_button.setBackground(getResources().getDrawable(R.drawable.breathing_deactive_button_background));
 
-                advance_breathing_button.setTextColor(getResources().getColor(R.color.colorRelaxationActiveTitle));
+                advance_breathing_button.setTextColor(getResources().getColor(R.color.colorBackground));
                 beginner_breathing_button.setTextColor(getResources().getColor(R.color.colorRelaxationTitleText));
+
+                Glide.with(this).load(R.drawable.breathing_b).into(imageView);
                 break;
             case R.id.beginner_breathing_button:
                 beginner_breathing_button.setBackground(getResources().getDrawable(R.drawable.breathing_active_button_background));
                 advance_breathing_button.setBackground(getResources().getDrawable(R.drawable.breathing_deactive_button_background));
 
-                beginner_breathing_button.setTextColor(getResources().getColor(R.color.colorRelaxationActiveTitle));
+                beginner_breathing_button.setTextColor(getResources().getColor(R.color.colorBackground));
                 advance_breathing_button.setTextColor(getResources().getColor(R.color.colorRelaxationTitleText));
+
+                Glide.with(this).load(R.drawable.breathing_a).into(imageView);
                 break;
         }
     }
